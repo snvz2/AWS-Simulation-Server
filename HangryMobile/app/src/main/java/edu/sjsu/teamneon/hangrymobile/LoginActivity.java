cea9e1b956c41d94011f904a4fd72e03594c41b3
aaronescoto <aaron.escoto@sjsu.edu>
2017-04-21 23:03:38 -0700

    button now switches accordingly to client UI

--- HangryMobile/app/src/main/java/edu/sjsu/teamneon/hangrymobile/LoginActivity.java
+++ HangryMobile/app/src/main/java/edu/sjsu/teamneon/hangrymobile/LoginActivity.java
@@ -3,6 +3,7 @@ package edu.sjsu.teamneon.hangrymobile;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,29 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.AsyncTask;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ListView;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
@@ -58,7 +82,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mProgressView;
     private View mLoginFormView;
     private VideoView myVideo;
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -77,6 +101,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
+        Button btn = (Button)findViewById(R.id.findTruck);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, FoodTruckLocator.class));
+            }
+        });
 
         // Set up the login form.
  /*       mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
