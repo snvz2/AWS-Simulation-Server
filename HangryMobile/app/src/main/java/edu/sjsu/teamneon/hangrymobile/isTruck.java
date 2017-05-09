3660342109ca04a80776b1a02a3970850132472c
aaronescoto <aaron.escoto@sjsu.edu>
2017-05-08 20:17:28 -0700

    basic ui layout

---
+++ HangryMobile/app/src/main/java/edu/sjsu/teamneon/hangrymobile/isTruck.java
@@ -0,0 +1,13 @@
+package edu.sjsu.teamneon.hangrymobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class isTruck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_is_truck);
+    }
+}
