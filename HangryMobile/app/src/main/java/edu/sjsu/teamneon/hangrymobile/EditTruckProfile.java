0c14252c49ee563a3ccdb08d01159cf059714f5b
aaronescoto <aaron.escoto@sjsu.edu>
2017-05-10 12:47:52 -0700

    added/prepped submit button for dynamo

--- HangryMobile/app/src/main/java/edu/sjsu/teamneon/hangrymobile/EditTruckProfile.java
+++ HangryMobile/app/src/main/java/edu/sjsu/teamneon/hangrymobile/EditTruckProfile.java
@@ -2,6 +2,8 @@ package edu.sjsu.teamneon.hangrymobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditTruckProfile extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class EditTruckProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_truck_profile);
+        Button btnSubmit = (Button)findViewById(R.id.submit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TODO: Add to Dynamo Description Field
+
+            }
+        });
     }
 }
