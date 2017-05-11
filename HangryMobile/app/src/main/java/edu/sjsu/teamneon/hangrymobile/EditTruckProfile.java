4a7460f40d90a3e2dbe02318630f651e95e895f9
aaronescoto <aaron.escoto@sjsu.edu>
2017-05-10 19:41:44 -0700

    added fields and dynamo logic in profile page

--- HangryMobile/app/src/main/java/edu/sjsu/teamneon/hangrymobile/EditTruckProfile.java
+++ HangryMobile/app/src/main/java/edu/sjsu/teamneon/hangrymobile/EditTruckProfile.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class EditTruckProfile extends AppCompatActivity {
 
@@ -13,13 +14,17 @@ public class EditTruckProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_truck_profile);
         Button btnSubmit = (Button)findViewById(R.id.submit);
+        final EditText descriptionVal = (EditText) findViewById(R.id.description);
+        final EditText truckNameVal = (EditText) findViewById(R.id.truckName1);
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String des = String.valueOf(descriptionVal.getText());
+                String name = String.valueOf(truckNameVal.getText());
                 //TODO: Add to Dynamo Description Field
-
-
+                new DynamoCRUD.updateTruckDescription(EditTruckProfile.this).execute("1", des);
+                new DynamoCRUD.updateTruckName(EditTruckProfile.this).execute("1", name);
             }
         });
         Button btnWebview = (Button)findViewById(R.id.webview);
